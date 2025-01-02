c09febf032a53e4f9e4ebdef1feed27c83fb4b1c
okuyamayusei <yusei.okym17@gmail.com>
2025-01-02 18:35:51 +0900

    ger_train_time

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,2 +1,7 @@
+from.yakusa_kudari import yakusa_to_kouzouzi_bp
+from.yakusa_nobori import yakusa_to_okazaki_bp
 # Blueprintをリストとしてまとめる
-blueprints = []
+blueprints = [
+    yakusa_to_kouzouzi_bp,
+    yakusa_to_okazaki_bp,
+]
